0786a3e6449f85c432176a06a4c9b2af1e7f7dac
Benjamin Chadwick <S00252669@atu.ie>
2025-01-07 20:06:40 +0000

    Added subclasses to Account and implemented abstract method

---
+++ CurrentAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrevExamPaper
+{
+    internal class CurrentAccount:Account
+    {
+        // Constants
+        private const decimal INTEREST_RATE = 0.03M;
+        // Constructors
+        public CurrentAccount(string firstName, string lastName, decimal balance, DateTime interestDate)
+            : base(firstName, lastName, balance, interestDate) { }
+        //methods
+        public override void CalculateInterest()
+        {
+           // Check if interest has been set in previous year
+           DateTime allowedDate = DateTime.Now.AddYears(-1);
+            if (allowedDate >= InterestDate)
+            {
+                Balance = Balance + (Balance + INTEREST_RATE);
+                InterestDate = DateTime.Now;
+            }
+        }
+    }
+}
---
+++ SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrevExamPaper
+{
+    internal class SavingsAccount : Account
+    {
+        // Constants
+        private const decimal INTEREST_RATE = 0.06M;
+        // Constructors
+        public SavingsAccount(string firstName, string lastName, decimal balance, DateTime interestDate)
+            : base(firstName, lastName, balance, interestDate) { }
+        //methods
+        public override void CalculateInterest()
+        {
+            // Check if interest has been set in previous year
+            DateTime allowedDate = DateTime.Now.AddYears(-1);
+            if (allowedDate >= InterestDate)
+            {
+                Balance = Balance + (Balance + INTEREST_RATE);
+                InterestDate = DateTime.Now;
+            }
+        }
+    }
+}
